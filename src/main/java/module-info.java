7b63d91f3768c21e849b454cf55b7903c2a18b1b
Tailee42 <tailee05@hotmail.fr>
2022-10-15 14:21:53 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module fr.isika.cda21.annuaire {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens fr.isika.cda21.annuaire to javafx.fxml;
+    exports fr.isika.cda21.annuaire;
+}
